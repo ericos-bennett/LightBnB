59589f5e62fe9e1793b5e6d498eca8426354cb2f
Eric <ericos.bennett@gmail.com>
2021-02-04 19:15:45 +0000

    connect user queries to psql db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -19,17 +19,13 @@ const users = require('./json/users.json');
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
 */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+const getUserWithEmail = email => {
+  const queryString = `SELECT * FROM users WHERE email = $1`;
+  const values = [email];
+  
+  return pool.query(queryString, values)
+    .then(res => res.rows.length ? res.rows[0] : null)
+    .catch(err => console.log(err));
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -38,22 +34,37 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+const getUserWithId = id => {
+  const queryString = `SELECT * FROM users WHERE id = $1`;
+  const values = [id];
+  
+  return pool.query(queryString, values)
+    .then(res => res.rows.length ? res.rows[0] : null)
+    .catch(err => console.log(err));
 };
 exports.getUserWithId = getUserWithId;
-
 
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+// const addUser =  function(user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// };
+
+const addUser = user => {
+  const queryString = `INSERT INTO users (name, email, password) 
+  VALUES ($1, $2, $3)
+  RETURNING *`;
+  const values = [user.name, user.email, user.password];
+  
+  return pool.query(queryString, values)
+    .then(user => user.rows[0])
+    .catch(err => console.log(err));
 };
 exports.addUser = addUser;
 
