f37528f1b1eb9c97592a3921a64681b3fc37f0ee
Eric <ericos.bennett@gmail.com>
2021-02-04 20:27:37 +0000

    connect getAllReservations to psql db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -58,8 +58,8 @@ exports.getUserWithId = getUserWithId;
 
 const addUser = user => {
   const queryString = `INSERT INTO users (name, email, password) 
-  VALUES ($1, $2, $3)
-  RETURNING *`;
+                       VALUES ($1, $2, $3)
+                       RETURNING *`;
   const values = [user.name, user.email, user.password];
   
   return pool.query(queryString, values)
@@ -72,11 +72,25 @@ exports.addUser = addUser;
 
 /**
  * Get all reservations for a single user.
- * @param {string} guest_id The id of the user.
+ * @param {string} guestId The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+const getAllReservations = function(guestId, limit = 10) {
+  const queryString = `
+    SELECT reservations.*, properties.*, AVG(rating) AS average_rating
+    FROM users
+    JOIN reservations ON users.id = guest_id
+    JOIN properties ON property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE users.id = $1 AND end_date < now()::date
+    GROUP BY reservations.id, properties.id
+    ORDER BY start_date
+    LIMIT $2;`;
+  const values = [guestId, limit];
+  
+  return pool.query(queryString, values)
+    .then(user => user.rows)
+    .catch(err => console.log(err));
 };
 exports.getAllReservations = getAllReservations;
 
